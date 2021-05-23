8922eb421af984b877fe7a32ab81dd674820a935
srabana <srabana.ac16@gmail.com>
2021-05-23 16:23:52 +0530

    modified fonts

--- js/cards.js
+++ js/cards.js
@@ -1,27 +1,6 @@
 // Display data in the cards
 
 async function fetchData() {
-<<<<<<< HEAD
-    await fetch("https://api.covid19api.com/summary")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        c='+';
-        const confirmed = data.Countries[76].TotalConfirmed;
-        const new_confirmed = data.Countries[76].NewConfirmed;
-        document.getElementById( "confirmed").innerHTML += `<p>${confirmed}<br><small>${c}${new_confirmed}</small></p>`; 
-        const recovered = data.Countries[76].TotalRecovered;
-        const new_recovered = data.Countries[76].NewRecovered;
-        document.getElementById("recovered").innerHTML += `<p>${recovered}<br><small>${c}${new_recovered}</small></p>`; 
-        const deceased = data.Countries[76].TotalDeaths;
-        const new_deceased = data.Countries[76].NewDeaths;
-        document.getElementById( "deceased").innerHTML += `<p>${deceased}<br><small>${c}${new_deceased}</small></p>`; 
-      });
-  }
-  fetchData();
-  
-=======
   await fetch("https://api.covid19api.com/summary")
     .then((response) => {
       // console.log("Inside first then");
@@ -36,21 +15,21 @@ async function fetchData() {
 
       document.getElementById(
         "confirmed"
-      ).innerHTML += `<p>${confirmed}<br><small>${c}${new_confirmed}</small></p>`;
+      ).innerHTML += `<p>${confirmed}<br><br><small style="font-weight: bold;color:blue;">${c}${new_confirmed}</small></p>`;
 
       const recovered = data.Countries[76].TotalRecovered;
       const new_recovered = data.Countries[76].NewRecovered;
 
       document.getElementById(
         "recovered"
-      ).innerHTML += `<p>${recovered}<br><small>${c}${new_recovered}</small></p>`;
+      ).innerHTML += `<p>${recovered}<br><br><small style="font-weight: bold;color:green;">${c}${new_recovered}</small></p>`;
 
       const deceased = data.Countries[76].TotalDeaths;
       const new_deceased = data.Countries[76].NewDeaths;
 
       document.getElementById(
         "deceased"
-      ).innerHTML += `<p>${deceased}<br><small>${c}${new_deceased}</small></p>`;
+      ).innerHTML += `<p>${deceased}<br><br><small style="font-weight: bold;color:red;">${c}${new_deceased}</small></p>`;
     });
 }
 
@@ -83,4 +62,3 @@ new Chart(document.getElementById("bar-chart"), {
     },
   },
 });
->>>>>>> 14d0ae67732223a391877d41edcb1574ac97dde1
