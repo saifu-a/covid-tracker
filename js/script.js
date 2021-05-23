ba07b207ece04b73a022d1acfdcc106e705eb379
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-05-23 19:01:50 +0530

    added line-chart

--- js/script.js
+++ js/script.js
@@ -1,22 +1,66 @@
-// Country Data
-
-const categories = [
-  "TotalConfirmed",
-  "TotalDeaths",
-  "TotalRecovered",
-  "NewConfirmed",
-  "NewDeaths",
-  "NewRecovered",
-];
+let confirmed = [];
+let deaths = [];
+let recovered = [];
+let dates = [];
 
 async function fetchIndiaData() {
-  let response = await fetch("https://api.covid19api.com/summary");
+  let response = await fetch(
+    "https://api.covid19api.com/total/dayone/country/india"
+  );
   let data = await response.json();
-  indiaData = data["Countries"][76];
 
-  for (let category of categories) {
-    console.log(category, indiaData[category]);
+  let length = data.length - 1;
+
+  for (let i = 10; i >= 0; i--) {
+    confirmed[10 - i] = data[length - i]["Confirmed"];
+    deaths[10 - i] = data[length - i]["Deaths"];
+    recovered[10 - i] = data[length - i]["Recovered"];
+    dates[10 - i] = data[length - i]["Date"];
   }
+  console.log(confirmed);
+  console.log(deaths);
+  console.log(recovered);
+  console.log(dates);
 }
 
 fetchIndiaData();
+
+let my_chart = document.getElementById("line-chart").getContext("2d");
+let covid_graph = new Chart(my_chart, {
+  type: "line",
+  data: {
+    labels: dates,
+    datasets: [
+      {
+        label: "Total Confirmed Cases",
+        data: confirmed,
+        fill: false,
+        borderColor: "#FFD700",
+        minBarLength: 100,
+      },
+      {
+        label: "Total Recovered",
+        data: recovered, //[300, 600, 800, 800],
+        fill: false,
+        borderColor: "#2E8B57",
+        minBarLength: 100,
+      },
+      {
+        label: "Total Deaths",
+        data: deaths, //[700, 800, 900, 1000],
+        fill: false,
+        borderColor: "#FF0000",
+        minBarLength: 100,
+      },
+    ],
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Graphical Representation",
+      fontSize: 25,
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  },
+});
