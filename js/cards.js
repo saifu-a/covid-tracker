e260635a5094792f4a5297d9d8161e68ed70f35f
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-06-13 11:20:23 +0530

    added counter animation

--- js/cards.js
+++ js/cards.js
@@ -3,46 +3,93 @@
 async function fetchData() {
   await fetch("https://api.covid19api.com/summary")
     .then((response) => {
-      // console.log("Inside first then");
       return response.json();
     })
     .then((data) => {
-      // console.log("Inside second then");
-      c = "+";
-      // console.log(data.Countries[76].NewConfirmed);
+      const confirmed_element = document.querySelector("#confirmed p");
+      const new_confirmed_element = document.querySelector("#confirmed small");
       const confirmed = data.Countries[76].TotalConfirmed;
       const new_confirmed = data.Countries[76].NewConfirmed;
 
-      document.getElementById(
-        "confirmed"
-      ).innerHTML = `<p>${confirmed.toLocaleString(
-        "en-IN"
-      )}<br><br><small style="font-weight: bold;color:blue;">${c}${new_confirmed.toLocaleString(
-        "en-IN"
-      )}</small></p>`;
+      confirmed_element.setAttribute("data-target", confirmed);
+      confirmed_element.classList.add("counter");
 
+      new_confirmed_element.setAttribute("data-target", new_confirmed);
+      new_confirmed_element.classList.add("new-counter");
+
+      const recovered_element = document.querySelector("#recovered p");
+      const new_recovered_element = document.querySelector("#recovered small");
       const recovered = data.Countries[76].TotalRecovered;
       const new_recovered = data.Countries[76].NewRecovered;
 
-      document.getElementById(
-        "recovered"
-      ).innerHTML = `<p>${recovered.toLocaleString(
-        "en-IN"
-      )}<br><br><small style="font-weight: bold;color:green;">${c}${new_recovered.toLocaleString(
-        "en-IN"
-      )}</small></p>`;
+      recovered_element.setAttribute("data-target", recovered);
+      recovered_element.classList.add("counter");
 
+      new_recovered_element.setAttribute("data-target", new_recovered);
+      new_recovered_element.classList.add("new-counter");
+
+      const deceased_element = document.querySelector("#deceased p");
+      const new_deceased_element = document.querySelector("#deceased small");
       const deceased = data.Countries[76].TotalDeaths;
       const new_deceased = data.Countries[76].NewDeaths;
 
-      document.getElementById(
-        "deceased"
-      ).innerHTML = `<p>${deceased.toLocaleString(
-        "en-IN"
-      )}<br><br><small style="font-weight: bold;color:red;">${c}${new_deceased.toLocaleString(
-        "en-IN"
-      )}</small></p>`;
+      deceased_element.setAttribute("data-target", deceased);
+      deceased_element.classList.add("counter");
+
+      new_deceased_element.setAttribute("data-target", new_deceased);
+      new_deceased_element.classList.add("new-counter");
+
+      counterAnimation();
+      newCounterAnimation();
     });
 }
 
 fetchData();
+
+// Counter Animation Logic
+
+function counterAnimation() {
+  const counters = document.querySelectorAll(".counter");
+  const speed = 200;
+
+  counters.forEach((counter) => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
+
+      const inc = target / speed;
+
+      if (count < target) {
+        counter.innerHTML = Math.ceil(count + inc);
+        setTimeout(updateCount, 1);
+      } else {
+        counter.innerText = `${target.toLocaleString("en-IN")}`;
+      }
+    };
+
+    updateCount();
+  });
+}
+
+function newCounterAnimation() {
+  const counters = document.querySelectorAll(".new-counter");
+  const speed = 200;
+
+  counters.forEach((counter) => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
+
+      const inc = target / speed;
+
+      if (count < target) {
+        counter.innerHTML = Math.ceil(count + inc);
+        setTimeout(updateCount, 1);
+      } else {
+        counter.innerText = `+${target.toLocaleString("en-IN")}`;
+      }
+    };
+
+    updateCount();
+  });
+}
